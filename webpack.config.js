cd0c2aa5503d43e759557f53e097909857494f27
Cheenee <cheenee.colaste24@gmai.com>
2017-04-08 19:06:56 +0800

    add HTML + Image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanPlugin= require('clean-webpack-plugin');
 /**this plugin will take the css code the put to separate css file**/
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var extractPlugin = new ExtractTextPlugin({
@@ -11,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname,'dist'),//use path because we need an absolute path
     filename: 'bundle.js',
-    publicPath: '/dist'
+    // publicPath: '/dist'
   },
   module:{
     /**to add rules in a modules**/
@@ -29,7 +31,7 @@ module.exports = {
         ]
       },
       {
-        /**check if css file**/
+        /**check if scss file**/
         test: /\.scss$/,
         /**
         * loader: to add a single loader
@@ -40,12 +42,34 @@ module.exports = {
         use: extractPlugin.extract({
           use:['css-loader','sass-loader']
         })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: '/'
+            }
+          }
+        ]
       }
     ]
   },
   plugins:[
-    extractPlugin
-    // /**dis plugins is to minfied the bundle code**/
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    /**this plugins is to clean or empty the specified folder**/
+    new CleanPlugin(['dist'])
+    // /**this plugins is to minfied the bundle code**/
     // new webpack.optimize.UglifyJsPlugin({
     //   //...
     // })
